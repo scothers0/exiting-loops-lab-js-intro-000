bce4316ae39ae9227364752589c32382df5d5403
harmonious-cache-6099 <isaac_scothern@hotmail.co.uk>
2020-04-09 19:24:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,5 +18,7 @@ function keepGoing(array, changeValue, skipValue) {
 }
 
 function findBy(array, findFn){
-  
+  for (let i = 0; i < array.length; i ++){
+    if(array[i] == findFn(array[i]))
+  }
 }
